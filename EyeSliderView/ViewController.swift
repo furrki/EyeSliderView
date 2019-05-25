35b7427999cbfe088cc6cce19a359791b9aa9f77
furrki <furkankaynary@gmail.com>
2019-05-25 03:23:25 +0300

    Initial Commit

---
+++ EyeSliderView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeSliderView
+//
+//  Created by Furkan Kaynar on 25.05.2019.
+//  Copyright © 2019 furrki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
